74f2b9bd237350da27c8d0a10af455434a814819
Yaelciny <mag07yael@gmail.com>
2026-06-26 10:15:44 -0600

    Integracion de boton de whatsapp

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Services from "@/components/Services/Services";
 import Projects from "@/components/Projects/Projects";
 import Contact from "@/components/Contact/Contact";
 import Footer from "@/components/Footer/Footer";
+import FloatingWhatsApp from "@/components/FloatingWhatsApp/FloatingWhatsApp";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Contact />
       </main>
       <Footer />
+      <FloatingWhatsApp />
     </>
   );
 }
